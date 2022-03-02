411ac234b7e5ee1bc5d350d48b3eae0b89602287
wellingtonaquino22 <wel201617@gmail.com>
2022-03-02 13:41:12 -0300

    Primeiro Commit

---
+++ src/main/java/com/primeiroprojeto/dio/springbootweb/Controller.java
@@ -0,0 +1,16 @@
+package com.primeiroprojeto.dio.springbootweb;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+	
+	@GetMapping("/")
+	public String mensagem() {
+		return "nosso primeiro projeto Spring boot web";
+	}
+
+}
